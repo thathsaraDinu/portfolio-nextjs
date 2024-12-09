79b4162294277c83ed6b2bdf8b8c02064391d7f1
thathsaraDinu <wthathsaradinuwan@gmail.com>
2024-12-09 23:48:40 +0530

    test-3

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -3,9 +3,9 @@ import dynamic from "next/dynamic";
 import ThemeContext from "@/context/theme-context";
 
 // Lazy load the components that are not immediately visible on page load
-const About = dynamic(() => import("@/components/about"));
-const Contact = dynamic(() => import("@/components/contact"));
+
 const Projects = dynamic(() => import("@/components/projects"));
+const Skills = dynamic(() => import("@/components/skills"));
 const Hero = dynamic(() => import("@/components/hero"));
 const NavBar = dynamic(() => import("@/components/navbar"));
 
@@ -29,14 +29,15 @@ export default function Wrapper() {
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
         <Hero theme={theme} />
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
-        <About />
+        
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
+        <Skills theme={theme} />
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
         <Education /> {/* Loaded immediately */}
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
         <Projects />
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
-        <Contact theme={theme} />
+        
         <hr className="max-w-screen-xl mx-auto dark:border-white border-black" />
         <Footer /> {/* Loaded immediately */}
       </div>
