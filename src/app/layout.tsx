877f79612fbefcdfb68edf48251bfc85803b3c6c
thathsaraDinu <wthathsaradinuwan@gmail.com>
2024-12-11 12:57:11 +0530

    Remove Inter font-face declarations from globals.css and implement Roboto font in layout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import { ThemeProvider } from "@/context/theme-context";
 import "./globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"], // Customize weights
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +18,7 @@ export default function RootLayout({
         <title>Thathsara Dinuwan Portfolio</title>
         <link rel="icon" href="/icons/logo-icon.webp" type="image/webp" />
       </head>
-      <body className="font-inter">
+      <body className={roboto.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
