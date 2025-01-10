31d4fade6ae86a6b684c2de2764274ceb55de6fe
thathsaraDinu <wthathsaradinuwan@gmail.com>
2025-01-10 15:39:27 +0530

    Refactor About component layout and styling for improved readability and alignment

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,20 +7,20 @@ const About: React.FC = () => {
       <div className="flex justify-center items-center lg:gap-0 max-w-screen-xl mx-auto">
         <ScrollAnimation
           initial={{ opacity: 0, x: -50 }}
-          className="flex flex-col items-start max-w-[600px] md:w-1/2   text-start gap-5 "
+          className="flex flex-col items-center max-w-[600px] md:w-full text-center gap-5 "
         >
           <div className="flex flex-col gap-2">
-            <div className="custom-top-topic text-left dark:text-lime-200">
+            <div className="custom-top-topic  dark:text-lime-200">
               ABOUT ME
             </div>
-            <div className="custom-second-topic text-left dark:text-blue-400">
+            <div className="custom-second-topic  dark:text-blue-400">
               Know who am I
             </div>
-            <div className="custom-third-topic text-left dark:text-blue-100">
+            <div className="custom-third-topic  dark:text-blue-100">
               My Journey in few words
             </div>
           </div>
-          <div className=" text-blue-950 dark:text-white  ">
+          <div className=" text-blue-950 dark:text-white">
             I’m Thathsara Dinuwan, a 23-year-old software engineering student
             at the Sri Lanka Institute of Technology. I specialize in full-stack
             development with a passion for Flutter, Node.js, and Next.js. I’m
@@ -29,17 +29,17 @@ const About: React.FC = () => {
             explore new technologies, I aim to create impactful solutions and
             enhance user experiences. Feel free to connect!
           </div>
-          <button className="transition-all text-sm font-semibold duration-200 rounded py-2 px-4 mt-4 dark:bg-slate-100 bg-blue-950  dark:text-blue-950 text-blue-100  dark:hover:bg-blue-300 hover:bg-blue-800">
+          {/* <button className="transition-all text-sm font-semibold duration-200 rounded py-2 px-4 mt-4 dark:bg-slate-100 bg-blue-950  dark:text-blue-950 text-blue-100  dark:hover:bg-blue-300 hover:bg-blue-800">
             Know more
-          </button>
+          </button> */}
           <hr className="max-w-screen-xl mx-auto h-2 border-white bg-white" />
         </ScrollAnimation>
-        <ScrollAnimation
+        {/* <ScrollAnimation
           className="md:block hidden w-1/3"
           initial={{ opacity: 0, x: 50 }}
         >
           <></>
-        </ScrollAnimation>
+        </ScrollAnimation> */}
       </div>
     </section>
   );
