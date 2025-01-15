6a39f20c1690775ebdf7fe599ab07fba312e1093
thathsaraDinu <wthathsaradinuwan@gmail.com>
2025-01-15 13:12:31 +0530

    update image

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,17 +19,16 @@ const About: React.FC = () => {
               </div>
             </div>
           </ScrollAnimation>
-          <ScrollAnimation
-            initial={{ opacity: 0, x: 0, y: 0, scale: 0.9 }}
-          >
-            <div className=" text-blue-950 dark:text-white">
-              I’m Thathsara Dinuwan, a 23-year-old software engineering student
-              at the Sri Lanka Institute of Technology. I specialize in
-              full-stack development with a passion for Flutter, Node.js, and
-              Next.js. I’m skilled in building both mobile and web apps, using
-              languages like Java, JavaScript, Python, and Dart. Always eager to
-              learn and explore new technologies, I aim to create impactful
-              solutions and enhance user experiences. Feel free to connect!
+          <ScrollAnimation initial={{ opacity: 0, x: 0, y: 0, scale: 0.9 }}>
+            <div className=" text-blue-950 dark:text-white text-lg">
+              I’m <span className="font-bold text-blue-900 dark:text-blue-300"> Thathsara Dinuwan</span>, a
+              23-year-old software engineering student at the Sri Lanka
+              Institute of Technology. I specialize in full-stack development
+              with a passion for Flutter, Node.js, and Next.js. I’m skilled in
+              building both mobile and web apps, using languages like Java,
+              JavaScript, Python, and Dart. Always eager to learn and explore
+              new technologies, I aim to create impactful solutions and enhance
+              user experiences. Feel free to connect!
             </div>
             {/* <button className="transition-all text-sm font-semibold duration-200 rounded py-2 px-4 mt-4 dark:bg-slate-100 bg-blue-950  dark:text-blue-950 text-blue-100  dark:hover:bg-blue-300 hover:bg-blue-800">
             Know more
