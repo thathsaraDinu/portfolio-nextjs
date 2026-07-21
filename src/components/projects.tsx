820182cb6f7c36e202de9fe286c68b46be218a1d
thathsara <wthathsaradinuwan@gmail.com>
2026-07-21 12:32:51 +0530

    update view

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -135,7 +135,6 @@ const Projects: React.FC = () => {
                   custom={index}
                   viewport={{ once: true }}
                   initial="initial"
-                  whileInView="animate" // Animate when in view
                   className="dark:bg-slate-900 bg-slate-200 group border dark:border-slate-600 border-slate-400 relative col-span-1 dark:text-blue-100 text-blue-950 rounded-md shadow-md z-10 overflow-hidden"
                 >
                   <div className="overflow-hidden h-[200px] w-full">
